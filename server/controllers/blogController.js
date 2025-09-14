629a3ea84c00c1bb8946b8e12dc756c8b18a702e
Yash Karande <yashkarande4240@gmail.com>
2025-09-14 13:13:31 +0530

    feat: Create API for blog list, details, delete, and update

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import imagekit from '../configs/imagekit.js';
-import Blog from '../models/Blog.js';
+import Blog from '../models/blog.js';
 
 export const addBlog = async (req, res) => {
   try{
@@ -37,6 +37,51 @@ export const addBlog = async (req, res) => {
 
     res.json({success: true, message: "Blog added successfully"})
     
+  } catch (error) {
+    res.json({success: false, message: error.message})
+  }
+}
+
+export const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({isPublished: true})
+    res.json({success: true, blogs})
+  } catch (error) {
+    res.json({success: false, message: error.message})
+  }
+}
+
+export const getBlogById = async (req, res) => {
+  try {
+    const { blogId } = req.params;
+    const blog = await Blog.findById(blogId)
+    if(!blog){
+      res.json({success: false, message: "Blog not found"});
+    }
+    res.json({success: true, blog})
+  } catch (error) {
+    res.json({success: false, message: error.message})
+  }
+}
+
+export const deleteBlogById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    await Blog.findByIdAndDelete(id);
+    res.json({success: true, message: 'Blog deleted successfully'})
+  } catch (error) {
+    res.json({success: false, message: error.message})
+  }
+}
+
+
+export const togglePublish = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const blog = await Blog.findById(id);
+    blog.isPublished = !blog.isPublished;
+    await blog.save();
+    res.json({success: true, message: 'Blog status updated'})
   } catch (error) {
     res.json({success: false, message: error.message})
   }
