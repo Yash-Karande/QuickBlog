99b02e5b1a5bebf7389d6f0f1798c99be482ef43
madhu12-c <mchanda2006@gmail.com>
2025-10-01 14:59:06 +0530

    fix-error

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Bloglist from '../components/Bloglist'
 import Newsletter from '../components/Newsletter'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
